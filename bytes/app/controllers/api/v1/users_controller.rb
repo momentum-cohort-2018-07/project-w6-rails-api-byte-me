0d07351803b72b0079307ac8b70292ae0b2ef2fa
josephrfarmer <josephr.farmer24@gmail.com>
2018-09-08 13:24:29 -0400

    Comments controller is finished

--- bytes/app/controllers/api/v1/users_controller.rb
+++ bytes/app/controllers/api/v1/users_controller.rb
@@ -1,12 +1,12 @@
 class Api::V1::UsersController < ApplicationController
-  def index
-  end
+  # def index
+  # end
 
   def create
   end
 
-  def update
-  end
+  # def update
+  # end
 
   def destroy
   end
