103652cc2ad5ed13d39599a1965b606ccfc1c9f9
JoannaDavidson <joanna@shrikesong.com>
2018-09-08 13:53:38 -0400

    Fleshed out sessions and users controllers

--- bytes/app/controllers/api/v1/sessions_controller.rb
+++ bytes/app/controllers/api/v1/sessions_controller.rb
@@ -1,4 +1,11 @@
 class Api::V1::SessionsController < ApplicationController
   def create
+    user = User.find_by_username(params[:username])
+
+    if user && user.authenticate(params[:password])
+      render json: { token: user.api_token }
+    else
+      render json: { error: "Invalid" }, status: :unauthorized
+    end
   end
 end
--- bytes/app/controllers/api/v1/users_controller.rb
+++ bytes/app/controllers/api/v1/users_controller.rb
@@ -2,12 +2,28 @@ class Api::V1::UsersController < ApplicationController
   #def index
   #end
 
+  def new
+    @user = User.new
+  end
+
   def create
+    @user = User.new(user_params)
+
+    if @user.save
+        render json: @user, status: :created 
+      else
+        render json: @user.errors, status: :unprocessable_entity
+    end
   end
 
   #def update
   #end
 
   def destroy
+    User.find(params[:id]).destroy
+  end
+
+  def user_params
+    params.require(:users).permit(:name, :username, :email, :password_digest)
   end
 end
